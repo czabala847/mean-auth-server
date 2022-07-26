86eebb614e9ac83ccc1f138b6a9f31c45eb4f624
Carlos Zabala <czabala847@gmail.com>
2022-07-26 17:21:17 -0500

    Configurando cors y obteniendo informacion en el controlador

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,9 @@
 const { response } = require("express");
 
 const createUser = (req, res = response) => {
+  const { email, name, password } = req.body;
+  console.log(email, name, password);
+
   return res.json({
     ok: true,
     msg: "Crear usuario /new",
@@ -8,6 +11,10 @@ const createUser = (req, res = response) => {
 };
 
 const login = (req, res = response) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+
   return res.json({
     ok: true,
     msg: "Login de usuario /",
