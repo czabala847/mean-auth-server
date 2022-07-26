40d59045ab005b761d005c4a8537b732d038210f
Carlos Zabala <czabala847@gmail.com>
2022-07-26 17:02:07 -0500

    Creando rutas de la aplicacion

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola desde nuevo");
+const express = require("express");
+
+//Crear el servidor/aplicación de express
+const app = express();
+
+//Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en puerto ${4000}`);
+});
